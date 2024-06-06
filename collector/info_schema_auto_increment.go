860d20579a2cdca78fd00e1ef2e48c538ce0a7f5
Jesse Dearing <code@me.jesse.dev>
2024-06-05 20:33:27 -0700

    fix: revert to query that works on 5.7

--- collector/info_schema_auto_increment.go
+++ collector/info_schema_auto_increment.go
@@ -24,17 +24,29 @@ import (
 )
 
 const infoSchemaAutoIncrementQuery = `
-		SELECT table_schema, table_name, column_name, auto_increment,
-		  pow(2, case data_type
-		    when 'tinyint'   then 7
-		    when 'smallint'  then 15
-		    when 'mediumint' then 23
-		    when 'int'       then 31
-		    when 'bigint'    then 63
-		    end+(column_type like '% unsigned'))-1 as max_int
-		  FROM information_schema.columns c
-		  STRAIGHT_JOIN information_schema.tables t USING (table_schema,table_name)
-		  WHERE c.extra = 'auto_increment' AND t.auto_increment IS NOT NULL
+SELECT
+    table_schema,
+    table_name,
+    column_name,
+    AUTO_INCREMENT,
+    pow(2, CASE data_type
+        WHEN 'tinyint' THEN
+            7
+        WHEN 'smallint' THEN
+            15
+        WHEN 'mediumint' THEN
+            23
+        WHEN 'int' THEN
+            31
+        WHEN 'bigint' THEN
+            63
+        END + (column_type LIKE '% unsigned')) - 1 AS max_int
+FROM
+    information_schema.tables t
+    JOIN information_schema.columns c USING (table_schema, table_name)
+WHERE
+    c.extra = 'auto_increment'
+    AND t.auto_increment IS NOT NULL
 		`
 
 // Metric descriptors.
